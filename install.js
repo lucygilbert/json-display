5210c7f18bf3eeed53305d5dff4b9f5b06ad7628
Simon A. Eugster <simon.eu@gmail.com>
2017-10-16 23:17:23 +0200

    Skip install.js when installing with npm install
    Fixes https://github.com/lucygilbert/json-display/issues/7

--- install.js
+++ install.js
@@ -1,6 +1,11 @@
-var Validate = require('git-validate');
-
+try {
+    var Validate = require('git-validate');
+} catch (e) {
+    console.log('Skipping install.js. (This is only needed when working on this package.)');
+    process.exit(0);
+}
 Validate.installScript('lint', "jshint ./**/*.js");
 Validate.installScript('test', "karma start");
 
 Validate.configureHook('pre-commit', ['lint', 'test']);
+
